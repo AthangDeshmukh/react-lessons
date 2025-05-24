fce59a7426d1831fff3dd1a7a44f3c2c37608f35
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-24 20:03:46 +0530

    update

---
+++ src/components/App6.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Fruit from "./Fruit";
+
+
+function App6() {
+  return (
+    <div className="fruit-grid">
+      <Fruit name="Apple" />
+      <Fruit name="Mango" />
+      <Fruit name="Orange" />
+      <Fruit name="Banana" />
+    </div>
+  );
+}
+
+export default App6;
---
+++ src/components/Fruit.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Fruit({ name }) {
+  return (
+    <div className="fruit-card">
+      <h3>{name}</h3>
+    </div>
+  );
+}
+
+export default Fruit;
