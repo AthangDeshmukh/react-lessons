1518d281282ac757250f8ad512aae152ec5ba833
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-24 19:43:12 +0530

    update

--- src/components/App2.jsx
+++ src/components/App2.jsx
@@ -1,25 +1,3 @@
-// import "./App2.css";
-// export default function App2() {
-//   const products = [
-//     { id: 1, name: "Product 1", price: 23 },
-//     { id: 2, name: "Product 2", price: 30 },
-//     { id: 3, name: "Product 3", price: 45 },
-//   ];
-//   return (
-//     <div>
-//       <h1>This is App2 component</h1>
-//       <div className="App-Product-Container">
-//         {products.map((value) => (
-//           <div className="App-Product-Box" key={value.id}>
-//             <h3>{value.name}</h3>
-//             <h4>{value.price}</h4>
-//             <p><button>Add to Cart</button></p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
 import "./App2.css";
 import Product from "./Product";
 export default function App2() {
@@ -31,9 +9,14 @@ export default function App2() {
   return (
     <div>
       <h1>This is App2 component</h1>
+      <h2>Working with Arrays and Objects,Applying Styles</h2>
       <div className="App-Product-Container">
         {products.map((value) => (
-          <Product value={value} />
+          <div className="App-Product-Box">
+            <h2>{value.name}</h2>
+            <h3>{value.price}</h3>
+            <button>Add to Cart</button>
+          </div>
         ))}
       </div>
     </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,5 @@
 export default function Product() {
-  return <div>Product</div>;
+  return <div>Products</div>;
+  
 }
+ 
