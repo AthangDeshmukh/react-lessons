c371c6dfb84898e3ba967a21e9d4f991adb8c67e
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-25 20:52:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
 import App6 from "./components/App6";
-import App7 from "./components/App7"; // Correct the casing to match the actual file name
+import App7 from "./components/App7";
 import App8 from "./components/App8";
-import App9 from "./components/App9"; 
- import App10 from "./components/App10"; // Import App10 component
+import App9 from "./components/App9";
+import App10 from "./components/App10";
+import App11 from "./components/App11";
+import App12 from "./components/App12";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -25,16 +27,17 @@ function App() {
         <Route path="/app2" element={<App2 />}></Route>
         <Route path="/app3" element={<App3 />}></Route>
         <Route path="/app4" element={<App4 />}></Route>
-        <Route path="/app5" element={<App5 />}></Route>
-        <Route path="/app6" element={<App6 />}></Route>
-        <Route path="/app7" element={<App7 />}></Route>
-        <Route path="/app8" element={<App8 />}></Route>
-        <Route path="/app9" element={<App9 />}></Route> 
-        <Route path="/app10" element={<App10 />}></Route> {/* Add route for App10 */}
+         <Route path="/app5" element={<App5 />}></Route>
+         <Route path="/app6" element={<App6 />}></Route>
+         <Route path="/app7" element={<App7 />}></Route>
+         <Route path="/app8" element={<App8 />}></Route>
+         <Route path="/app9" element={<App9 />}></Route>
+         <Route path="/app10" element={<App10 />}></Route>
+         <Route path="/app11" element={<App11 />}></Route>
+         <Route path="/app12" element={<App12 />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 }
-
 export default App;
