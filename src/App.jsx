fefa124e2ecba4d42d3dd318d5ed22e2a37bcd12
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-25 17:46:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
 import App6 from "./components/App6";
-import App7 from "./components/App7";
+import App7 from "./components/App7"; // Correct the casing to match the actual file name
+import App8 from "./components/App8";
+import App9 from "./components/App9"; // Import App9
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -22,12 +24,15 @@ function App() {
         <Route path="/app2" element={<App2 />}></Route>
         <Route path="/app3" element={<App3 />}></Route>
         <Route path="/app4" element={<App4 />}></Route>
-         <Route path="/app5" element={<App5 />}></Route>
-         <Route path="/app6" element={<App6 />}></Route>
-         <Route path="/app7" element={<App7 />}></Route>
+        <Route path="/app5" element={<App5 />}></Route>
+        <Route path="/app6" element={<App6 />}></Route>
+        <Route path="/app7" element={<App7 />}></Route>
+        <Route path="/app8" element={<App8 />}></Route>
+        <Route path="/app9" element={<App9 />}></Route> {/* Add App9 route */}
       </Routes>
       <Footer />
     </BrowserRouter>
   );
 }
+
 export default App;
