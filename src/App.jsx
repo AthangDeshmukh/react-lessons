8bca8f4a79cc70e6dfde4ed7ffc79fae02e10e59
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-27 19:10:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,12 @@ import App13 from "./components/App13";
 import App14 from "./components/App14";
 import App15 from "./components/App15";
 import App16 from "./components/App16";
+import App17 from "./components/App17";
+import App18 from "./components/App18";
+import App19 from "./components/App19";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
@@ -43,6 +47,9 @@ function App() {
           <Route path="/app14" element={<App14 />}></Route>
           <Route path="/app15" element={<App15 />}></Route>
            <Route path="/app16" element={<App16 />}></Route>
+            <Route path="/app17" element={<App17 />}></Route>
+             <Route path="/app18" element={<App18 />}></Route>
+             <Route path="/app19" element={<App19 />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
