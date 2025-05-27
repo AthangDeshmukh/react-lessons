0ec3b79aafb2b9601b0924ebd093c98fa330b0cc
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-27 20:47:58 +0530

    update

--- src/components/App20b.jsx
+++ src/components/App20b.jsx
@@ -1,26 +1,16 @@
-import React, { useState, createContext } from "react";
-import App20a from "./App20a";
-import App20b from "./App20b";
+import React, { useContext } from "react";
+import { AppContext } from "./App20";
 
-export const AppContext = createContext();
-
-export default function App20() {
-  const [count, setCount] = useState(0);
+export default function App20b() {
+  const { count, setCount } = useContext(AppContext);
 
   return (
     <div>
-      <h1>App20</h1>
-      <h2>useContext hook - sharing variables across components</h2>
+      <h3>App20b</h3>
       <p>{count}</p>
       <p>
         <button onClick={() => setCount(count + 1)}>Update Count</button>
       </p>
-      <hr />
-      <AppContext.Provider value={{ count, setCount }}>
-        <App20a />
-        <hr />
-        <App20b />
-      </AppContext.Provider>
     </div>
   );
 }
